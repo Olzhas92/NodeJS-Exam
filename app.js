aad5ebc241745a141bf7a876cd04385318b69744
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-12-14 19:41:43 +0600

    endpoints added

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/files", (request, response) => {
+  response.send("<h2>My Files</h2>");
+});
+
+app.use((request, response) => {
+  response.status(404).send("<h2>Page Not Found</h2>");
+});
 
 app.listen(7001, () => {
   console.log("server has been launched...");
